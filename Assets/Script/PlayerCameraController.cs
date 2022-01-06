60fe212e0d6237547871fadc7ef4173a08b7e848
JulienCodron <78977430+JulienCodron@users.noreply.github.com>
2022-01-06 21:43:47 +0100

    character + camera + controle
    Implement main charater controle and also camera for a FPS game.

---
+++ Assets/Script/PlayerCameraController.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] private float lookSensitibity;
+    [SerializeField] private float smoothing;
+
+    private GameObject player;
+    private Vector2 smoothedVelocity;
+    private Vector2 currentLookingPos;
+
+    private void Start()
+    {
+        player = transform.parent.gameObject;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        RotateCamera();
+    }
+
+    private void RotateCamera()
+    {
+        Vector2 inputValues = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+        inputValues = Vector2.Scale(inputValues, new Vector2(lookSensitibity * smoothing, lookSensitibity * smoothing));
+
+        smoothedVelocity.x = Mathf.Lerp(smoothedVelocity.x, inputValues.x, 1f / smoothing);
+        smoothedVelocity.y = Mathf.Lerp(smoothedVelocity.y, inputValues.y, 1f / smoothing);
+        currentLookingPos += smoothedVelocity;
+
+        transform.localRotation = Quaternion.AngleAxis(-currentLookingPos.y, Vector3.right);
+        player.transform.localRotation = Quaternion.AngleAxis(currentLookingPos.x, player.transform.up);
+        currentLookingPos.y = Mathf.Clamp(currentLookingPos.y, -80f, 80f);
+    }
+
+}
+
